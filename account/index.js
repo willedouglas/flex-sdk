08573daa374d45c1c921c668d2936a1f09b40823
Douglas Wille <willedouglas10@gmail.com>
2018-02-25 22:01:52 -0300

    Added company

--- account/index.js
+++ account/index.js
@@ -5,21 +5,13 @@ const default_headers = require('../comum/default_headers.js');
 const request = require('../comum/flex_request.js');
 
 class Account {
-  constructor(token) {
+  constructor(company_id, token) {
+    this.company_id = company_id;
     this.token = token;
     this.default_options = {
       'json': true,
       'headers': default_headers(this)
     };
-  }
-
-  login(data) {
-    data = data || {};
-    const url = `${config.api_url}/authenticate`;
-    const method = 'POST';
-
-    const options = Object.assign({}, this.default_options, { url, method, data });
-    return request(options);
   }
 
   getCompanies() {
@@ -30,8 +22,25 @@ class Account {
     return request(options);
   }
 
-  getUserInformation(token) {
-    const url = `${config.api_url}/authenticate/${token}`;
+  getCompany() {
+    const url = `${config.api_url}/${this.company_id}`;
+    const method = 'GET';
+
+    const options = Object.assign({}, this.default_options, { url, method });
+    return request(options);
+  }
+
+  login(data) {
+    data = data || {};
+    const url = `${config.api_url}/${this.company_id}/authenticate`;
+    const method = 'POST';
+
+    const options = Object.assign({}, this.default_options, { url, method, data });
+    return request(options);
+  }
+
+  getUserInformation() {
+    const url = `${config.api_url}/${this.company_id}/authenticate/${this.token}`;
     const method = 'GET';
 
     const options = Object.assign({}, this.default_options, { url, method });
