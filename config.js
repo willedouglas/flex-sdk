cc4dbbd0848920a35f0e4b86a543c3a240330e03
Douglas Wille <willedouglas10@gmail.com>
2018-03-18 15:17:43 -0300

    Adjusted API_URL to AWS path

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 
 module.exports = {
-	"api_url"     : process.env.API_URL || "http://localhost:3030",
+	"api_url" : process.env.API_URL || "http://api-flex.us-east-2.elasticbeanstalk.com",
 	"request_attempts": process.env.REQUEST_ATTEMPTS || 3,
 };
